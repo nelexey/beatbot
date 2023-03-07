e8f0806344f60e32c46b9056f88f91fb56cb6b41
Rancorouseful <80785407+Rancorouseful@users.noreply.github.com>
2023-03-07 22:52:55 +0300

    v2.1
    Добавлены новые сэмплы.
    Обновлен make_beat.py (добавлена возможность синхронизировать leads и help_leads)

--- make_beat.py
+++ make_beat.py
@@ -8,13 +8,16 @@ def jersey_club():
     bass = random.choice([AudioSegment.from_wav(file) for file in glob("style_JC/basses/*.wav")])
     hi_hat = random.choice([AudioSegment.from_wav(file) for file in glob("style_JC/hi-hats/*.wav")])
     kick = random.choice([AudioSegment.from_wav(file) for file in glob("style_JC/kicks/*.wav")])
-    clap = random.choice([AudioSegment.from_wav(file) for file in glob("style_JC/kicks/*.wav")])
+    clap = random.choice([AudioSegment.from_wav(file) for file in glob("style_JC/claps/*.wav")])
     
+    ## sync leads and help_leads ON
     leads_sync = random.randint(1, len(glob("style_JC/leads/*.wav")))
     lead = random.choice([AudioSegment.from_wav(file) for file in glob(f"style_JC/leads/lead{leads_sync}.wav")])
     help_lead = random.choice([AudioSegment.from_wav(file) for file in glob(f"style_JC/help_leads/helplead{leads_sync}.wav")])
-
-    print(leads_sync)
+    
+    ## sync leads and help_leads OFF
+    # lead = random.choice([AudioSegment.from_wav(file) for file in glob(f"style_JC/leads/*.wav")])
+    # help_lead = random.choice([AudioSegment.from_wav(file) for file in glob(f"style_JC/help_leads/*.wav")])
 
     ##### ТАКТЫ #####
 
@@ -25,14 +28,15 @@ def jersey_club():
     ## 2 ##
     sandwich2 = sandwich1
     sandwich2 = sandwich2.overlay(help_lead, position=0)
+    sandwich2 = sandwich2.overlay(clap, position=0)
     sandwich2 = sandwich2.overlay(hi_hat, position=0)
 
     overlay = overlay.append(sandwich2)
 
     ## 3 ##
     sandwich3 = sandwich2
-    sandwich3 = sandwich3.overlay(bass, position=0)
     sandwich3 = sandwich3.overlay(kick, position=0)
+    sandwich3 = sandwich3.overlay(bass, position=0)
     
     overlay = overlay.append(sandwich3)
 
@@ -41,7 +45,6 @@ def jersey_club():
     ## 5 ##
 
     ## 6 ##
-
 
     file_handle = overlay.export("output.wav", format="wav")
 
